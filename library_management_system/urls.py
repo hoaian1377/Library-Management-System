6cf5732899bb51a3bea277c9979f968519f55c12
LeKhanhThien0302 <thienpro1356@gmail.com>
2026-03-17 14:20:00 +0700

    up

--- library_management_system/urls.py
+++ library_management_system/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.base, name='base'),
+    path('books/', views.book, name='book'),
+    path('borrows/', views.borrow, name='borrow'),
+    path('borrowers/', views.borrower, name='borrower'),
+    path('reports/', views.report, name='report'),
 ]
--- library_management_system/views.py
+++ library_management_system/views.py
@@ -3,3 +3,11 @@ from django.http import HttpResponse
 # Create your views here.
 def base(request):
     return render(request,'base.html')
+def book(request):
+    return render(request,'book.html')
+def borrow(request):
+    return render(request,'borrow.html')
+def borrower(request):
+    return render(request,'borrower.html')
+def report(request):
+    return render(request,'report.html')
