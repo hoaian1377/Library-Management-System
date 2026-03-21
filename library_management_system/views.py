cb27541c6d2eb3f41769093382815d95fee127a1
nguyenphuongsang <sangnp3251@ut.edu.vn>
2026-03-21 23:47:15 +0700

    up thêm phần quản lý sách

--- library_management_system/views.py
+++ library_management_system/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book
 # Create your views here.
 def base(request):
     return render(request,'base.html')
 def book(request):
-    return render(request,'book.html')
+    books = Book.objects.all() # Lấy tất cả sách từ database
+    return render(request,'book.html' , {'books': books}) # Truyền qua HTML
 def borrow(request):
     return render(request,'borrow.html')
 def borrower(request):
