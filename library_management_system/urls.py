3e9df09b2775c17546610f629e1b78461e18391c
hoaian1377 <tommyjan1000@gmail.com>
2026-04-08 23:05:43 +0700

    upload qls + dnhap dky

--- library_management_system/urls.py
+++ library_management_system/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.base, name='base'),
-    path('books/', views.book, name='book'),
+    path('books/', views.book_list, name='book_list'),
     path('borrows/', views.borrow, name='borrow'),
     path('borrowers/', views.borrower, name='borrower'),
     path('reports/', views.report, name='report'),
+    path('login/', views.login_view, name='login_modal'),
+    path('logout/', views.logout_view, name='logout')
 ]
--- library_management_system/views.py
+++ library_management_system/views.py
@@ -1,15 +1,45 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Taikhoan, Sach
 # Create your views here.
 def base(request):
     return render(request,'base.html')
-def book(request):
-    books = Book.objects.all() # Lấy tất cả sách từ database
-    return render(request,'book.html' , {'books': books}) # Truyền qua HTML
 def borrow(request):
     return render(request,'borrow.html')
 def borrower(request):
     return render(request,'borrower.html')
 def report(request):
     return render(request,'report.html')
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = Taikhoan.objects.filter(username=username, matkhau=password).first()
+        if user:
+            request.session['user'] = username
+            return redirect('base')
+        else:
+            messages.error(request, "Sai tài khoản hoặc mật khẩu")
+
+    return render(request, "login_modal.html")
+  
+def logout_view(request):
+    request.session.flush()
+    return redirect('base')
+
+
+######################### Sach #######################
+
+def book_list(request):
+    books = Sach.objects.select_related('tacgiaid').all()
+
+    # SEARCH
+    keyword = request.GET.get('q')
+    if keyword:
+        books = books.filter(ten_sach__icontains=keyword)
+
+    return render(request, 'book.html', {
+        'books': books
+    })
